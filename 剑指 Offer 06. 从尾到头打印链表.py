ec14669d99e23019a37554eb16a67528ef23f045
Yun (Charles) YE <34646454+Charlesyyun@users.noreply.github.com>
2021-07-02 15:14:38 +0800

    剑指 Offer 06. 从尾到头打印链表
    简单

---
+++ 剑指 Offer 06. 从尾到头打印链表.py
@@ -0,0 +1,39 @@
+'''
+剑指 Offer 06. 从尾到头打印链表
+输入一个链表的头节点，从尾到头反过来返回每个节点的值（用数组返回）。
+
+ 
+
+示例 1：
+
+输入：head = [1,3,2]
+输出：[2,3,1]
+ 
+
+限制：
+
+0 <= 链表长度 <= 10000
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/cong-wei-dao-tou-da-yin-lian-biao-lcof
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+'''
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def reversePrint(self, head: ListNode) -> List[int]:
+        if head==None:
+            return []
+        else:
+            li = [head.val]
+            next_node = head.next
+            while next_node!=None:
+                li.append(next_node.val)
+                next_node = next_node.next
+            li.reverse()
+            return li
